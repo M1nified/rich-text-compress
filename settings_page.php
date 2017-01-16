3d66d91df12e7eb1a51d4b4fcf44205f08063956
M!nified <gora.pub@gmail.com>
2017-01-16 09:41:06 +0100

    added page selection - list

--- settings_page.php
+++ settings_page.php
@@ -61,6 +61,7 @@ if($widget_id != ''){
     WHERE WidgetId = '{$widget_id}' AND `Type` = 'content'");
     // print_r($widgets);
     foreach ($widgets as $key => $widget) {
+        $uniqid = uniqid();
         echo "<form method=\"POST\" action=\"#\">";
         // echo "<h3>{$widget->WidgetId}</h3>";
         echo '<p>';
@@ -70,7 +71,9 @@ if($widget_id != ''){
         echo '</p>';
         $display_on = $widget->DisplayOn;
         $display_on = preg_replace('/(^,|,$)/','',str_replace("!",",",$display_on));
-        echo "<p><label style=\"display:flex;align-items:center;\"><b>Display on: </b><input type=\"text\" name=\"DisplayOn\" value=\"{$display_on}\" style=\"flex-grow:2;\"></label></p>";
+        echo "<p><label style=\"display:flex;align-items:center;\"><b>Display on: </b><input class=\"rtc-displayon\" type=\"text\" name=\"DisplayOn\" value=\"{$display_on}\" style=\"flex-grow:2;\" data-group-id=\"{$uniqid}\"></label></p>";
+        echo "<p><button type=\"button\" class=\"button button-primary rtc-change-list\" data-group-id=\"{$uniqid}\">Select pages</button></p>";
+        echo "<p class=\"rtc-post-list-spot\" data-group-id=\"{$uniqid}\"></p>";
         echo "<p><input type=\"submit\" class=\"button button-primary widget-control-save right\"></p><div class=\"clear\"></div>";
         echo "<input type=\"hidden\" name=\"rtc_id\" value=\"{$widget->Id}\">";
         echo "<input type=\"hidden\" name=\"mode\" value=\"edit\">";
@@ -79,21 +82,70 @@ if($widget_id != ''){
     }
     echo "<a name=\"rtc_add\"></a><form method=\"POST\" action=\"#\">";
     echo "<h3>Add new: {$widget->WidgetId}</h3>";
+    $uniqid = uniqid();
     echo '<p>';
     wp_editor('','content');
     echo '</p>';
-    echo "<p><label style=\"display:flex;align-items:center;\"><b>Display on: </b><input type=\"text\" name=\"DisplayOn\" value=\"\" style=\"flex-grow:2;\"></label></p>";
+    echo "<p><label style=\"display:flex;align-items:center;\"><b>Display on: </b><input class=\"rtc-displayon\" type=\"text\" name=\"DisplayOn\" value=\"\" style=\"flex-grow:2;\" data-group-id=\"{$uniqid}\"></label></p>";
     echo "<input type=\"hidden\" name=\"WidgetId\" value=\"{$widget->WidgetId}\">";
     echo "<input type=\"hidden\" name=\"mode\" value=\"add\">";
+    echo "<p><button type=\"button\" class=\"button button-primary rtc-change-list\" data-group-id=\"{$uniqid}\">Select pages</button></p>";
+    echo "<p class=\"rtc-post-list-spot\" data-group-id=\"{$uniqid}\"></p>";
     echo "<p><input type=\"submit\" class=\"button button-primary widget-control-save right\"></p><div class=\"clear\"></div>";
     echo "</form>";
 }
-
 ?>
+<div id="rtc-post-list-container" style="display:none;">
+    <select multiple size="10" id="rtc-post-list" data-group-id="">
+    <?php
+    $pages_to_display_on = $wpdb->get_results("SELECT
+        ID, post_title
+        FROM {$wpdb->posts}
+        WHERE post_type = 'page'
+        ORDER BY ID");
+    foreach($pages_to_display_on as $post){
+        $id = str_replace("~","&nbsp;",str_pad($post->ID,7,"~"));
+        echo "<option value=\"{$post->ID}\">{$id}{$post->post_title}</option>";
+    }
+    ?>
+    </select>
+</div>
 <script type="text/javascript">
     jQuery("#rtc-select-module").on('change',function(){
         console.log(this)
         let search = location.search.replace(/[&?]widget_id=[^&#]*/im,'');
         location = location.origin + location.pathname + search + (search == '' ? '?' : '&') + 'widget_id=' + this.value;
+    });
+</script>
+<script>
+// Display on
+    if(typeof $ === 'undefined'){
+        var $ = jQuery;
+    }
+    jQuery(function(){
+        $("button.rtc-change-list").on('click',function(evt){
+            var group_id = $(this).data('group-id');
+            var list = $("#rtc-post-list");
+            $(list).data('group-id',group_id);
+            $('.rtc-post-list-spot[data-group-id="'+group_id+'"]').append(list);
+            var listposts = $("#rtc-post-list>option");
+            $(listposts).removeAttr('selected');
+            var input_display = $('input.rtc-displayon[data-group-id="'+group_id+'"]');
+            var page_ids = $(input_display).first().val().split(",");
+            console.log(input_display);
+            console.log(page_ids);
+            page_ids.forEach(function(pageid) {
+                if(pageid){
+                    $(listposts).filter('[value='+pageid+']').attr('selected',true);
+                }
+            }, this);
+
+        });
+        $("select#rtc-post-list").on('change',function(){
+            var list = $(this).val();
+            var group_id = $(this).data('group-id');
+            var input_display = $('input.rtc-displayon[data-group-id="'+group_id+'"]');
+            $(input_display).first().val(list.join(','));
+        });
     });
 </script>
